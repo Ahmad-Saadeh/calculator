5bf614d79e39d18338b2c4742246ccbab7a6e3ba
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-05 23:27:36 +0200

    calculator code

--- calculator.py
+++ calculator.py
@@ -1,10 +1,23 @@
-
 def main():
-	#write your code here
-	pass
-	
 
-
+  firstNumber = input("Enter the first number: ")
+  secondNumber = input("Enter the second number: ")
+  operation = input("Choose one of the operations (*, /, +, -) ")
+  if firstNumber.isdigit() and secondNumber.isdigit():
+    firstNumber = int(firstNumber)
+    secondNumber = int(secondNumber)
+    if operation == "*":
+        print("The answer is {}".format(firstNumber * secondNumber))
+    elif operation == "/":
+        print("The answer is {}".format(firstNumber / secondNumber))
+    elif operation == "+":
+        print("The answer is {}".format(firstNumber + secondNumber))
+    elif operation == "-":
+        print("The answer is {}".format(firstNumber - secondNumber))
+    else:
+        print("Invalid Operation!")
+  else:
+    print("Invalid Numbers!")
 
 if __name__ == '__main__':
 	main()
